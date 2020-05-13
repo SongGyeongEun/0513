8f972cb5a854738dffc0c4b370e7b7e5ad13badd
SongGyeongEun <62276767+SongGyeongEun@users.noreply.github.com>
2020-05-13 10:49:35 +0900

    Add files via upload

---
+++ revstr.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+	char str[80];
+	int i;
+	
+	printf("문자열 입력:  ");
+	scanf("%s", str);
+	
+	for(i = strlen(str); i >= 0; i--)
+		putchar(str[i]);
+	
+	return 0;
+}
---
+++ skl.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+int main()
+{
+	
+	returon 0;
+}
